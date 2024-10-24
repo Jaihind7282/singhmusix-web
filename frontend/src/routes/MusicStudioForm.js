5644da8f4219e06323de1b53a3a165d27462cea3
Jaihind Kumar <162858463+Jaihind7282@users.noreply.github.com>
2024-10-24 11:30:07 +0530

    MusicStudioForm.js

--- frontend/src/routes/MusicStudioForm.js
+++ frontend/src/routes/MusicStudioForm.js
@@ -25,7 +25,7 @@ const MusicStudioForm = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/register", {
+      const response = await fetch("https://singhmusix-web.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
